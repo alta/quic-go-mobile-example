cf48fcdf3d526ad8b02cd1fe1ffab590b3672836
Randy Reddig <ydnar@shaderlab.com>
2021-07-16 11:59:52 -0700

    internal/tools: add gobind

--- internal/tools/tools.go
+++ internal/tools/tools.go
@@ -4,8 +4,10 @@ package tools
 
 import (
 	// Blank imports for tool binaries
+	_ "golang.org/x/mobile/cmd/gobind"
 	_ "golang.org/x/mobile/cmd/gomobile"
 )
 
 //go:generate go install golang.org/x/mobile/cmd/gomobile
+//go:generate go install golang.org/x/mobile/cmd/gobind
 //go:generate gomobile init
