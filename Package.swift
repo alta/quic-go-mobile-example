d63987b6e1916ce177cc4b67aadd753557cce242
Randy Reddig <randy@alta.software>
2021-07-21 11:03:25 -0700

    Package.swift: remove platforms
    It doesn’t look like platform declarations are explicitly necessary.

--- Package.swift
+++ Package.swift
@@ -3,10 +3,6 @@ import PackageDescription
 
 let package = Package(
 	name: "EchoKit",
-	platforms: [
-		.macOS(.v11),
-		.iOS(.v13),
-	],
 	products: [
 		// Products define the executables and libraries a package produces, and make them visible to other packages.
 		.library(
